ca6c0f7da563b00ea96e30dfb3245d5421511a60
Ricardo <ricardoinliner@yahoo.com>
2021-04-27 12:21:26 -0300

    parsing body from prismic

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,8 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { RichText } from "prismic-dom"
+import Head from 'next/head'
+import Header from '../../components/Header'
+import { FaCalendar, FaUser } from 'react-icons/fa'
 
 
 import { getPrismicClient } from '../../services/prismic';
@@ -7,12 +10,17 @@ import { getPrismicClient } from '../../services/prismic';
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+
 interface Post {
   first_publication_date: string | null;
   data: {
     title: string;
     banner: {
       url: string;
+      alt: string;
     };
     author: string;
     content: {
@@ -28,10 +36,65 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post() {
-  
+export default function Post({ post: rawPost }: PostProps) {
+
+  const post = {
+    createdAt: format(
+      new Date(rawPost.first_publication_date),
+      "dd LLL yyyy", {
+      locale: ptBR
+    }),
+    title: rawPost.data.title,
+    banner: rawPost.data.banner,
+    author: rawPost.data.author,
+    duration: 10,
+    body: RichText.asHtml(rawPost.data.content[0].body)
+  }
+
+
   return (
-    <div>Ola post</div>
+
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <Header />
+
+      <main className={styles.container} >
+
+        <div className={styles.banner}>
+          <img src={post.banner.url} alt={post.banner.alt} />
+        </div>
+
+        <div className={styles.post}>
+
+          <div>
+            <strong>
+              {post.title}
+            </strong>
+            <div className={styles.info}>
+              <span>
+                <FaCalendar />
+                <time>{post.createdAt}</time>
+              </span>
+
+              <span>
+                <FaUser />
+                <p>{post.author}</p>
+              </span>
+            </div>
+
+            <div
+              className={styles.content}
+              dangerouslySetInnerHTML={{
+                __html: post.body
+              }}
+            />
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
 
@@ -42,37 +105,24 @@ export const getStaticPaths = async () => {
   // TODO
 
 
-  return { 
+  return {
     paths: [],
     fallback: 'blocking'
-}
+  }
 
 };
 
 export const getStaticProps: GetStaticProps = async context => {
   const prismic = getPrismicClient();
 
+  const { params: { slug } } = context
 
-  const { params : { slug } } = context
+  const post = await prismic.getByUID('posts', String(slug), {})
 
-  console.log("slug", slug)
-
-  const response = await prismic.getByUID('posts', String(slug), {})
-  
-  console.log("resposta",response)
-
-
-  const post = {
-      slug,
-      title: response.data.title,
-      subtitle: response.data.subtitle,
-      author: response.data.author,
-
-  }
-
-  return { props : {
+  return {
+    props: {
       post
-  }}
-
+    }
+  }
 
 };
